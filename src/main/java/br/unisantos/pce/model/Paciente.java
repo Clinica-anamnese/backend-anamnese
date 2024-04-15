24e46bb07da89d0ccd8e658d9d2a17ecc8762609
João Pedro Máximo <joao.maximo@fatec.com>
2024-04-15 18:25:14 -0300

    add paciente model

---
+++ src/main/java/br/unisantos/pce/model/Paciente.java
@@ -0,0 +1,46 @@
+package br.unisantos.pce.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Paciente {
+
+    private enum Sexo {
+        masculino,
+        feminino,
+        nao_informado;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "nome", length = 60, nullable = false)
+    private String nome;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "sexo", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Sexo sexo;
+
+    @Column(name = "ativo", nullable = false)
+    private boolean ativo;
+
+}
